c547f72a2b6c054c64defbc4e10fdb94cc6d2a96
Oscar Jimenez <ojimenezlmpc@gmail.com>
2021-03-26 09:29:33 -0700

    Added colliders, health, and on click spawn

--- Glitch Garden/Assets/Scripts/Projectile.cs
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50;
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider) 
+    {
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+
+        if(attacker && health)
+        {
+            health.DealDamage(damage);    
+            Destroy(gameObject);
+        }
+
+        
+    } 
 }
